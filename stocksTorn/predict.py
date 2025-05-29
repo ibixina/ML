09d8f9ab0357d70b4267b158b116f9d9e6c12f33
nao <theshishir01@gmail.com>
2025-05-29 14:27:17 -0500

    pretty printing

--- stocksTorn/predict.py
+++ stocksTorn/predict.py
@@ -5,6 +5,10 @@ import os, requests, json
 # load models
 models = {}
 
+RESET = '\033[0m'
+RED = "\033[91m"
+GREEN = "\033[92m"
+
 model_dir = "./models"
 for file in os.listdir(model_dir):
     if file.endswith(".pkl"):
@@ -14,6 +18,20 @@ for file in os.listdir(model_dir):
             models[model_name] = pickle.load(f)
 
 
+def print_row(data, type = 0, spacing = 10, color = RESET):
+# type 0: row, type 1: header, type 2: footer
+    row ="|"
+    for d in data:
+        row += str(d).rjust(spacing) + " |"
+
+    if type == 1:
+        print("-" * len(row))
+
+    print(color + row + RESET)
+
+    if type == 1:
+        print("-" * len(row))
+    
 
 def get_data(ticker, period = "h6"):
     url = f"https://tornsy.com/api/{ticker}?interval={period}"
@@ -28,10 +46,11 @@ def get_data(ticker, period = "h6"):
 
 
 period = "d1"
+headers = ["Stock", "Current", "Predicted", "Change %", "ModelName"]
+print_row(headers, type=1)
 for modelName in models:
     if period not in modelName: continue
     stockName = modelName.split("_")[0]
-    print(f"Predicting for {stockName} using model {modelName}")
     df = get_data(stockName, period)
 
     # generate 10 lags for the latest data
@@ -43,14 +62,21 @@ for modelName in models:
     latest_data = df.tail(1).drop(columns=["Timestamp", "Opening Price", "Closing Price", "High Price", "No of Shares"])
     
     # predict
-    prediction = models[modelName].predict(latest_data)
+    prediction = models[modelName].predict(latest_data)[0]
     current_price = float(latest_data["Low Price"].values[0])
 
-    change = float(prediction[0]) - current_price
+    change = float(prediction) - current_price
     changeperc = (change / current_price) * 100
-    print(f"Current: {current_price}, Predicted: {prediction[0]}, Change: {changeperc:.2f}%")
+
+    color = RESET
+    if changeperc > 0.01:
+        color = GREEN
+    elif changeperc < -0.03:
+        color = RED
+
+    row = [stockName, current_price, round(prediction, 2), "{:.2f}".format(changeperc), modelName]
+    print_row(row, color = color)
     if changeperc > 0.3:
         print(f"####  Buy {stockName} ####")
-    # break
 
 
